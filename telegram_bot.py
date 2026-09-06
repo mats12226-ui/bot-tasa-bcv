e9f0f5cf7c39bda906cb090662a444cb56304752
mats12226-ui <mats12226@gmail.com>
2026-09-06 13:20:42 -0400

    Orden de definicion de funcion run_http_server y soporte HEAD

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,13 +9,19 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(b"Bot activo 24/7")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
 def run_http_server():
-    port = int(os.environ.get("PORT", 8080))
-    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
-    server.serve_forever()
+        port = int(os.environ.get("PORT", 8080))
+        server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
+        server.serve_forever()
 
 threading.Thread(target=run_http_server, daemon=True).start()
 
